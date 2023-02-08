4fbc03c0142c2c3e0f790d6e59c12d2f4a046414
MONESH-M-S <moneshsanju@gmail.com>
2023-02-08 17:52:35 +0530

    Added login, signup with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+		loadChildren: () => import("./pages/login/login.module").then((m) => m.LoginModule),
+  },
+  {
+    path: 'signup',
+		loadChildren: () => import("./pages/signup/signup.module").then((m) => m.SignupModule),
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
