02bafad70f9ebfb05ceef861654364e0e251641a
P-aveen06 <paveenpradeep2@gamil.com>
2023-02-09 22:09:12 +0530

    added login page

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { RouterModule, Routes } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
+import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
+import {PasswordModule} from 'primeng/password';
+import {CheckboxModule} from 'primeng/checkbox';
 const routes: Routes = [
   {path: '', component: LoginComponent}
 ]
@@ -13,7 +17,13 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    ButtonModule,
+    InputTextModule,
+    PasswordModule,
+    CheckboxModule,
     RouterModule.forChild(routes)
+
   ]
 })
 export class LoginModule { }
