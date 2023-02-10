5899ec36b9c2a694b86f39321e946a6abd578095
MONESH-M-S <moneshsanju@gmail.com>
2023-02-10 14:11:53 +0530

    Login page with auth completed

---
+++ src/app/pages/login/login.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/core/models/user';
+import { environment } from 'src/environments/environment.development';
+import { UserLogin } from './User-Login';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  BACKEND_URL = environment.BACKEND_URL;
+
+  constructor(private http: HttpClient) { }
+
+  loginUser(userLogin: UserLogin): Observable<{ user: string, token: string, message: string }> {
+    return this.http.post<{ user: string, token: string, message: string }>(`${this.BACKEND_URL}/user/login`, userLogin)
+  }
+}
