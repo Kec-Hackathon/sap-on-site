15675339a490a046d7552f5139b719ac6f12b464
monesh <monesh.ms@freightify.com>
2023-02-10 16:49:28 +0530

    verifed signup data

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup,FormBuilder,Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Signup } from './signup-form.data';
 import { MessageService } from 'primeng/api';
 @Component({
@@ -8,36 +8,54 @@ import { MessageService } from 'primeng/api';
   styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent {
-  signup=Signup;
-  form!:FormGroup;
-  loading:boolean=false;
+  signup = Signup;
+  form!: FormGroup;
+  loading: boolean = false;
   uploadedFiles: any[] = [];
-  constructor(private formBuilder: FormBuilder ) { }
+  displayImage: string = '';
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.form = this.formBuilder.group({
-      name:['',Validators.required],
+      name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      roll_no:['',Validators.required],
+      roll_no: ['', Validators.required],
       password: ['', [Validators.required]],
-      department:['', Validators.required],
-      mentor_id:['', Validators.required],
-      year:['',Validators.required],
-      profile:['', Validators.required]
+      department: ['', Validators.required],
+      mentor_id: ['', Validators.required],
+      year: ['', Validators.required],
+      profile: ['', Validators.required],
     });
   }
 
-    load() {
-        this.loading = true;
-        setTimeout(() => this.loading = false, 1000);
-    }
-    onBasicUploadAuto(event:any) {
-      for(let file of event.files) {
-          this.uploadedFiles.push(file);
-      }
+  onSubmit() {
+    console.log(this.form.value);
 
+    if(this.form.valid) {
+      const formData = new FormData();
+      formData.append('name', this.form.value.name);
+      formData.append('email', this.form.value.email);
+      formData.append('password', this.form.value.password);
+      formData.append('roll_no', this.form.value.roll_no);
+      formData.append('department', this.form.value.department);
+      formData.append('mentor_id', this.form.value.mentor_id);
+      formData.append('year', this.form.value.year);
+      formData.append('profile', this.form.value.profile);
+    }
+    
+    this.loading = true;
+    setTimeout(() => (this.loading = false), 1000);
   }
 
+  onUpload(event: any) {
+    const file = event.files[0];
+    this.form.patchValue({ profile: file });
+    this.form.get('profile')!.updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.displayImage = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+  }
 }
-
-
